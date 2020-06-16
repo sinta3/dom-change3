5ae7dff76e51f2a3fa1b6893bf144c1b292275de
sinta3 <sinta.tri38@gmail.com>
2020-06-16 18:45:37 +0700

    add change

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let text1 = document.getElementById("text1");
 text1.remove();
 let text2 = document.getElementById("text2");
-text2.style.margin = "600px";
-text2.style.marginTop = "10px";
 let content = document.getElementById("content");
-content.style.margin = "auto";
+content.style.textAlign = "center";

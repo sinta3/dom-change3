b8ca955d645825201df4eca534c05043fd736aa0
sinta3 <sinta.tri38@gmail.com>
2020-06-16 16:19:01 +0700

    add all file

---
+++ script.js
@@ -0,0 +1,7 @@
+let text1 = document.getElementById("text1");
+text1.remove();
+let text2 = document.getElementById("text2");
+text2.style.margin = "600px";
+text2.style.marginTop = "10px";
+let content = document.getElementById("content");
+content.style.margin = "auto";
